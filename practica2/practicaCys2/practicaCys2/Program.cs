627a9d67776964e356f279fa12b5865a07f388b5
tomas <twm1@alu.ua.es>
2024-09-25 14:02:03 +0200

    dios quiera q esto funcione

--- practica2/practicaCys2/practicaCys2/Program.cs
+++ practica2/practicaCys2/practicaCys2/Program.cs
@@ -14,6 +14,7 @@ namespace practicaCys2
     {
         /// <summary>
         ///  The main entry point for the application.
+        ///  
         /// </summary>
         [STAThread]
         static void Main()
